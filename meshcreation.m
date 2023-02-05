93e4d9625ab6004a5b2fc448ddc1122129108b90
Tornes23 <nestor.uriarte@digipen.edu>
2023-02-05 11:28:06 +0100

    Mesh creation function

---
+++ meshcreation.m
@@ -0,0 +1,18 @@
+function t = meshcreation(meshtype, nodes)
+    if meshtype == 0
+        t = linspace(0, nodes - 1, nodes);
+    endif
+
+    if meshtype == 1
+        t = linspace(0, 1, nodes);
+    endif
+
+    if meshtype == 2
+        for i=1:nodes
+        t(i) = -cos(pi * (i-1) / nodes);
+        endfor
+    else
+        printf("Not a valid mesh type digit");
+    endif
+    t
+endfunction
