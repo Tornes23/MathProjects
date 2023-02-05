54a6c346f993eef2a05c75482d9fa6908e6ea307
David Miranda Pazos <m.david@digipen.edu>
2023-02-05 11:59:33 +0100

    Update gaussjordanmethod.m

--- gaussjordanmethod.m
+++ gaussjordanmethod.m
@@ -8,6 +8,65 @@
 %
 % Script input data for the first project
 %========================================================
-function gaussjordanmethod(PX, PY, PZ, dimension, outNodes, t, n, meshtype)
-{
-}
+function gaussjordanmethod(PX, PY, PZ, dimension, outNodes, t, n, meshType)
+
+    % Compute the coefficients for PX, PY and PZ when needed
+    cx = computeCoefficients(PX, t, n);
+    cy = computeCoefficients(PY, t, n);
+    if(dimension > 2)
+      cz = computeCoefficients(PZ, t, n);
+    endif
+
+
+    % Create an intervale based on the mesh type
+    intervalMin = 0;
+    intervalMax = n;
+
+    if(meshType == 2)
+      intervalMin = -1;
+    if(meshType == 1 || meshType == 2)
+      intervalMax = 1;
+
+    % Create intermediate t values inside the interval
+    steps = linspace(intervalMin, intervalMax, n);
+
+    % Compute x and y interpolated values inside the interval
+    xResults = polyval(cx', steps);
+    yResults = polyval(cy', steps);
+
+    % Compute z values only when needed
+    if(dimension > 2)
+      zResults = polyval(cz', steps);
+      plot3(xResults, yResults, zResults, 'r');
+      hold on;
+      plot3(PX, PY, PZ, 'ok');
+      hold on;
+      endfunction
+    endif
+
+    plot(xResults, yResults, 'r');
+    hold on;
+    plot(PX, PY, 'ok');
+    hold on;
+endfunction
+
+function c = computeCoefficients( P, t, n)
+
+    matrix = zeros(n: n + 1);
+
+    % Fill the matrix with the given points
+    for i = 1 : n
+      for j = 1 : n
+        matrix(i,j) = t(i)^(j-1);
+      endfor
+    endfor
+
+    % Add the augmented matrix's colimn
+    matrix(:,n+1) = P;
+
+    % Compute the result and extract it
+    matrix = rref(matrix);
+
+    c = matrix(:,n+1);
+
+endfunction
