ecbdd8034fd532bc4235e6266ea5acde1d7a643e
Daniel-Herreros <d.herreros@digipen.edu>
2023-02-11 16:54:01 +0100

    Changed the mesh Lagrange call in interpolation

--- interpolation.m
+++ interpolation.m
@@ -20,7 +20,7 @@ switch (methoddigit)
 case 0
   gaussjordanmethod(PX, PY, PZ, dimension, outputnodes, t, n);
 case 1
-   lagrangemethod(PX,PY,PZ,dimension,meshdigit,outputnodes)
+   lagrangemethod(PX,PY,PZ,dimension,outputnodes,t)
 case 2
   newtonmethod(t, PX, PY, PZ, outputnodes, dimension);
 endswitch
