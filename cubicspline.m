a2196e15bfb19f8f6bf28c6142d7ce6ae6ee27cb
David Miranda Pazos <davidmirandadev@gmail.com>
2023-03-06 16:56:53 +0100

    Ready to just plot

--- cubicspline.m
+++ cubicspline.m
@@ -10,23 +10,32 @@
 % of the given data.
 %========================================================
 function cubicspline
-  
+
   input;
-   
+
+  % get the amount of input points
   n = columns(PX);
-  
-  %t = linspace(0, n, n +1);
-  t = [-1 0 2 4];
-  
-  resultMatrix = zeros(n + 2, n + 4);
-  
+
+  % create a regular mesh
+  t = linspace(0, n, n +1);
+
+  % create a zero matrix which will hold the result
+  % create n + 2 rows and  n + 4 (or 5 if we have z)
+  % as the solution is given by
+  % a_0 -> a_(n + 2) and we add two more constrains
+  % so that is a unique solution
+  if(Dimension == 3)
+    resultMatrix = zeros(n + 2, n + 5);
+  else
+    resultMatrix = zeros(n + 2, n + 4);
+  endif
   % insert the standard basis components
   % a0 + a1*t + a2*t^2 + a3*t^3
   for(i = 1 : n)
     for(j = 1 : 4)
       resultMatrix(i, j) = t(i)^(j - 1);
+    endfor
   endfor
-endfor
 
   % insert the right shifted basis components
   for(i = 1 : n)
@@ -36,44 +45,34 @@ endfor
   endfor
 
   % add the additional constrains
-  
+  % p''(t0) = p''(tn) = 0
+  % as p''(t0) is p''(0) we can simplify it as 2*a_2 = 0
   resultMatrix(n + 1, 3) = 2;
-  resultMatrix(n + 1, 4) = -6; % remove this it is hardcoded from example
-  resultMatrix(n + 2, 3) = 2;
+
+  % to simplify p''(tn)
   resultMatrix(n + 2, 4) = 6 * t(n);
-  
+  resultMatrix(n + 2, 3) = 2;
+
   for(j = 5 : n + 2)
     resultMatrix(n + 2, j) = (t(n) - t(j - 3))*6;
   endfor
-  
-  
-  PX'
-  resultMatrix(1:n, n + 3) = PX'
-  resultMatrix(1:n, n + 4) = PY'
-  
+
+  resultMatrix(1:n, n + 3) = PX';
+  resultMatrix(1:n, n + 4) = PY';
+  if(Dimension == 3)
+    resultMatrix(1:n, n + 5) = PZ';
+  endif
+
   resultMatrix = rref(resultMatrix);
-  
+
   xCoef = resultMatrix(:, n + 3)';
   yCoef = resultMatrix(:, n + 4)';
-  if(dimension == 3)
+
+  if(Dimension == 3)
     zCoef = resultMatrix(:, n + 5)';
   endif
-  
-  % Create the input based based on the node number
-  mesh = linspace(0, outputnodes, outputnodes);
-  
-  
-  X = xCoef(1) + xCoef(2) * mesh + xCoef(3) * mesh.^2 +  xCoef(4) * mesh.^3;
-  Y = yCoef(1) + yCoef(2) * mesh + yCoef(3) * mesh.^2 +  yCoef(4) * mesh.^3;
-  if(dimension == 3)
-    Z = zCoef(1) + zCoef(2) * mesh + zCoef(3) * mesh.^2 +  zCoef(4) * mesh.^3;
-  endif
-  
-  if(dimension == 2)
-    plot(X,Y,'b');
-  elseif(dimension == 3)
-    plot3(X,Y,Z,'b');
-  endif
-  
-  
+
+  % Create the input based on the node number
+  m = linspace(0, n - 1, outputnodes);
+
 endfunction
