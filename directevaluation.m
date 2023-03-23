15f3f6c1af5a7d6634b1dcc825f26914c7663247
Daniel-Herreros <d.herreros@digipen.edu>
2023-03-23 18:18:48 +0100

    Working on it

---
+++ directevaluation.m
@@ -0,0 +1,14 @@
+function directevaluation()
+  input
+  
+  t = linspace(0,1,outputnodes);
+  n = colimns(PX);
+  for i = 0:n 
+  B = (n, i)power((1 - t),n-i)*power(t,i);
+  
+  
+  
+  
+  
+  
+endfunction
